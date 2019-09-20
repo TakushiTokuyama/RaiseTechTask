4626f8900f9fb152dd13b78a0c98df003ac7ec92
Takushi Tokuyama <sokoninoruna@gmail.com>
2019-09-20 16:36:32 +0900

    ④True or False ClassMethod

---
+++ twoWeek/Method.java
@@ -0,0 +1,29 @@
+package twoWeek;
+
+public class Method {
+
+	public static boolean execute(int i) {
+		if(i == 1) {
+		return true;
+		}else if(i == 0) {
+	    return false;
+		}else{
+	    return false;
+		}
+
+	}
+
+	public static void executeTwo(int i) {
+		if(i == 0) {
+			System.out.println("偽");
+		}else if(i == 1) {
+			System.out.println("真");
+		}else {
+			System.out.println("0か1を入力してね");
+		}
+	}
+
+
+	}
+
+
